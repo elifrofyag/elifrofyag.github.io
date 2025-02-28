df0780ddd6069150bdabea3ac5315fb88d9abf09
elifrofyag <annguyenvanb3@gmail.com>
2025-02-28 16:51:58 +0700

    add songs and color

--- db/song.js
+++ db/song.js
@@ -4,42 +4,37 @@ export default {
             name: "Sau tất cả",
             singer: "Erik",
             path: "assets/song/SauTatCa.mp3",
-            image:
-                "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/covers/5/5/55a2e33a5d4d6a70f5144181c28eacb0_1452855672.jpg"
+            image:"https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/covers/5/5/55a2e33a5d4d6a70f5144181c28eacb0_1452855672.jpg"
         },
         {
-            name: "Yêu em rất nhiều",
-            singer: "Hoàng Tôn",
-            path: "assets/song/YeuEmRatNhieu.mp3",
-            image:
-                "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/6/8/0/7/6807770ea5dc38ceaaa3a70810d804b4.jpg"
+            name: "we fell in love in october",
+            singer: "girl in red",
+            path: "assets/song/WeFellInLoveInOctober.mp3",
+            image:"https://m.media-amazon.com/images/I/91aq3lrAMfL._UF894,1000_QL80_.jpg"
         },
         {
-            name: "Có hẹn với thanh xuân",
-            singer: "MONSTAR & Grey D",
-            path: "assets/song/CoHenVoiThanhXuan.mp3",
-            image:
-                "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/e/2/3/f/e23ff2faaa64eebfc57e0acde247f0db.jpg"
+            name: "barbie&ken",
+            singer: "kate gill",
+            path: "assets/song/barbie&ken.mp3",
+            image:"https://i.scdn.co/image/ab67616d0000b2739a8e56dae6cb6494663d00b8"
         },
         {
             name: "Lần Hẹn Hò Đầu Tiên",
             singer: "Huyền Tâm Môn",
             path: "assets/song/LanHenHoDauTien.mp3",
-            image:
-                "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/7/e/9/4/7e94b95743998a108ff33beed5bae5c3.jpg"
+            image:"https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/7/e/9/4/7e94b95743998a108ff33beed5bae5c3.jpg"
         },
         {
             name: "Lệ Lưu Ly",
             singer: "Vũ Phụng Tiên & DT",
             path: "assets/song/LeLuuLy.mp3",
-            image:
-                "https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/6/9/3/f/693f8f516bfaa717ef4043f11edfdde2.jpg"
+            image:"https://photo-resize-zmp3.zmdcdn.me/w94_r1x1_jpeg/cover/6/9/3/f/693f8f516bfaa717ef4043f11edfdde2.jpg"
         },
         {
             name: "Em không",
             singer: "Vũ Thanh Vân",
-            image: "./assets/img/song5.jpg",
-            path: "./assets/songs/EmKhong.mp3"
-          },
+            image: "assets/img/song5.jpg",
+            path: "assets/song/EmKhong.mp3"
+        },
     ]
 }
